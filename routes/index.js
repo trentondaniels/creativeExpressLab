75b8e3059e867755f7b5f65f406e4e5ad61f3aed
Connor Cook <cojoco@users.noreply.github.com>
2015-11-12 04:36:52 +0000

    Added possible post route to routes

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,27 @@ router.get('/home', ensureAuthenticated, function(req, res) {
   res.sendfile('./public/home.html');
 });
 
+router.post('/addComment', ensureAuthenticated, function(req, res) {
+  var jsonData = "";
+  req.on('data', function(chunk) {
+    jsonData += chunk;
+  });
+  req.on('end', function() {
+    var reqObj = JSON.parse(jsonData);
+    MongoClient.connect("mongodb://localhost/creativeExpress", function(err, db) {
+      if(err) throw err;
+      db.collection('movies')
+        .update(
+          {name:reqObj.movieTitle},/*This should find the movie object by title*/
+          {$push: { comments: {text:reqObj.commentText,user:reqObj.user}}},/*This should define what to push in the document's array*/
+          function(err, records) {
+            res.writeHead(200);
+            res.end("");
+        });
+    } 
+  }
+});
+
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
 res.redirect('/')
